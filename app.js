829b495a2e7ac0abc69c16268eec6570bacbf054
Nour-1412 <nourelain796@gmail.com>
2026-07-09 22:07:58 +0300

    Remove PDF.js worker source URL

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /* ==========================================
    WEBBAG PDF WATERMARK STATE
 ========================================== */
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
+
 const appState = {
 
     /* -------------------------
