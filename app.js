3ac79bc9891886f372e03d98e22c99c82ab27b38
Nour-1412 <nourelain796@gmail.com>
2026-07-10 19:21:47 +0300

    Change PDF.js worker source version
    Updated PDF.js worker source to version 3.11.174.

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 /* ==========================================
    WEBBAG PDF WATERMARK STATE
 ========================================== */
-if(
-    window.pdfjsLib
-){
-
-    pdfjsLib.GlobalWorkerOptions.workerSrc =
-        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs";
-
-}
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 const appState = {
 
     /* -------------------------
