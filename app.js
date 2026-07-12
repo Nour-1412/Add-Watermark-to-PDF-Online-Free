4d4fa73f97d0e13e3ba6c4ecbb4cbf274a521136
Nour-1412 <nourelain796@gmail.com>
2026-07-12 21:57:34 +0300

    Uncomment PDF.js worker source line

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
    PDF.JS WORKER
 ========================================== */
 alert("APP.JS WORKING");
-pdfjsLib.GlobalWorkerOptions.workerSrc =
+//pdfjsLib.GlobalWorkerOptions.workerSrc =
 "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.js";
 
 /* ==========================================
